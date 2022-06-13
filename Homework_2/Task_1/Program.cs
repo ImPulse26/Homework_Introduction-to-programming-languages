e294fea0279d3e84daa702d85b3b04b579c393e7
Roman <nero26@bk.ru>
2022-06-13 12:11:12 +0300

    Homework_2 Task_1 complete

---
+++ Homework_2/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.Write("Введите трехзначное число => ");
+string srt = Console.ReadLine();
+int number = int.Parse(srt);
+int result = number % 100 / 10;
+
+ if ((number < 99) || (number > 1000)) {
+    Console.WriteLine($"Число {number} меньше 100 или больше 1000");
+ }
+ else Console.WriteLine($"Вторая цифра числа {number}: {result}");
---
+++ Homework_2/Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Введите трехзначное число => ");
+string srt = Console.ReadLine();
+int number = int.Parse(srt);
+int result = number % 100 / 10;
+
+ if ((number < 99) || (number > 1000)) {
+    Console.WriteLine($"Число {number} меньше 100 или больше 1000");
+ }
+ else Console.WriteLine($"Вторая цифра числа {number}: {result}");
