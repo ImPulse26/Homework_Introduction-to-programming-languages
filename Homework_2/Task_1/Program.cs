e393361317c1576bd195b0fc4e0e48ff4760f467
Roman <nero26@bk.ru>
2022-06-13 12:13:04 +0300

    Homework_2 Task_1 complete correction_1

--- Homework_2/Task_1/Program.cs
+++ Homework_2/Task_1/Program.cs
@@ -5,7 +5,5 @@ string srt = Console.ReadLine();
 int number = int.Parse(srt);
 int result = number % 100 / 10;
 
- if ((number < 99) || (number > 1000)) {
-    Console.WriteLine($"Число {number} меньше 100 или больше 1000");
- }
+ if ((number < 99) || (number > 1000)) Console.WriteLine($"Число {number} меньше 100 или больше 1000");
  else Console.WriteLine($"Вторая цифра числа {number}: {result}");
