f8db1540f2b4dfbaf75715d1d65d724ff982a998
Roman <nero26@bk.ru>
2022-06-13 15:03:34 +0300

    Homework_2 Task_4 complete

---
+++ Homework_2/Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+
+Console.Write("Введите число => ");
+string srt = Console.ReadLine();
+int number = int.Parse(srt);
+
+int result1 = number % 10;
+int result2 = number % 100 / 10;
+int result3 = number % 1000 / 100;
+
+if (number < 99) Console.WriteLine($"Третьей цифры нет");
+if (number > 99 && number < 1000) Console.WriteLine($"Третья цифра {result1}");
+if (number > 999 && number < 10000) Console.WriteLine($"Третья цифра {result2}");
+if (number > 9999 && number < 100000) Console.WriteLine($"Третья цифра {result3}");
---
+++ Homework_2/Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.Write("Введите день недели => ");
+string str = Console.ReadLine();
+int number = int.Parse(str);
+
+if (number >= 1 && number <= 5)
+{
+    Console.WriteLine($"День недели {number}, рабочий день.");
+}
+else if (number == 6 || number == 7)
+{
+    Console.WriteLine($"День недели {number}, выходной.");
+}
+else
+{
+    Console.WriteLine($"Дня недели {number}, нет.");
+}
