2109b725cd92c00fc47190ca8d0de1217a61de91
Roman <nero26@bk.ru>
2022-06-13 12:44:45 +0300

    Homework_2 Task_2 complete

--- Homework_2/Task_2/Program.cs
+++ Homework_2/Task_2/Program.cs
@@ -1,11 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
 Console.Clear();
-Console.Write("Введите трехзначное число => ");
-string srt = Console.ReadLine();
-int number = int.Parse(srt);
-int result = number % 100 / 10;
-
- if ((number < 99) || (number > 1000)) {
-    Console.WriteLine($"Число {number} меньше 100 или больше 1000");
- }
- else Console.WriteLine($"Вторая цифра числа {number}: {result}");
+int number = new Random().Next(100, 999);
+Console.WriteLine($"Число: {number}");
+int numberMN = number / 100 * 10 + number % 10;
+Console.WriteLine($"Число без втрой цифры: {numberMN}");
