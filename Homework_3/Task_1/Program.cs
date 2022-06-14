739e218825c8ee2443c9d1e020721213e75060c5
Roman <nero26@bk.ru>
2022-06-14 18:11:46 +0300

    Homework_3 Task_1 complete

---
+++ Homework_3/Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int number = Prompt("Введите пятизначное число => ");
+
+int[] array = new int[5];
+array[0] = number / 10000;
+array[1] = number % 10000 / 1000;
+array[2] = number % 1000 / 100;
+array[3] = number % 100 / 10;
+array[4] = number % 10;
+
+if (array[0] == array[4] && array[1] == array[3]) Console.WriteLine($"Введеное число ЯВЛЯЕТСЯ палиндромом");
+else Console.WriteLine($"Введеное число НЕ является палиндромом");
