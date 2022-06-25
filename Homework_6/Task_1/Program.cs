41f43fd003b803f75aba6e02dfe7e937d2d30359
Roman <nero26@bk.ru>
2022-06-25 12:36:15 +0300

    Homework_6 Task_1 and Task_2 complete

---
+++ Homework_6/Task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int[] numbers(int M)
+{
+    int[] MNumbers = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        MNumbers[i] = Prompt($"Введите {i + 1} из {M} чисел => ");
+    }
+    return MNumbers;
+}
+
+int PositiveNumber(int[] array)
+{
+    int count = 0;
+    foreach (int element in array)
+    {
+        if (element > 0) count++;
+    }
+    return count;
+}
+
+int M = Prompt($"Сколько чисел вывести? => ");
+int[] myNumbers = numbers(M);
+int countPositiveNumber = PositiveNumber(myNumbers);
+Console.WriteLine($"Положительных чисел => {countPositiveNumber}");
+
+
---
+++ Homework_6/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+double b1 = Prompt($"Введите b1 => ");
+double k1 = Prompt($"Введите k1 => ");
+double b2 = Prompt($"Введите b2 => ");
+double k2 = Prompt($"Введите k2 => ");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+if (x == y) Console.WriteLine($"Точка пересечения: ({x:0.0} ; {y:0.0})");
+else Console.WriteLine($"Точки пересечения не существует: ({x:0.0} ; {y:0.0})");
