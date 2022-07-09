f25c7a605b4c2acfafdb07118003450a44446f1b
Roman <nero26@bk.ru>
2022-07-09 09:36:01 +0300

    Homework_9 Task_1_2 complete

---
+++ Homework_9/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(System.Console.ReadLine());
+}
+
+void PrintNatural(int M, int N)
+{
+    if (M > N)
+    {
+        return;
+    }
+    if (N % 2 == 1)
+    {
+        N--;
+    }
+    PrintNatural(M, N-2);
+    System.Console.Write($"{N};");
+}
+
+PrintNatural(Prompt("write M => "), Prompt("write N => "));
---
+++ Homework_9/Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(System.Console.ReadLine());
+}
+
+int PrintSumNatural(int M, int N)
+{
+    if (M > N)
+    {
+        return 0;
+    }
+    return PrintSumNatural(M, N-1)+N;
+}
+
+int M = Prompt("write M => ");
+int N = Prompt("write N => ");
+
+int sum = PrintSumNatural(M, N);
+System.Console.WriteLine($"Cумма натуральных элементов в промежутке от {M} до {N} равна => {sum}");
